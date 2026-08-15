48f182c66ec2f9a37cf599c7177168a8eb55554a
AffectedArc07 <25063394+AffectedArc07@users.noreply.github.com>
2026-08-15 14:22:38 +0100

    All DBs now on main cluster

--- services/paracron/custom/scripts/invoke_all_sql_backups.sh
+++ services/paracron/custom/scripts/invoke_all_sql_backups.sh
@@ -6,10 +6,10 @@ echo "[$(date)] SQL Backup Started"
 # Forums
 backup_mysql.sh --host 172.28.0.14 --user backup_user --password ${MYSQL_BACKUP_PW} --dbname paradise_forums --backuppath /data/mysql_backups/forums
 # Game DB
-backup_mysql.sh --host 10.0.0.10 --user backup_user --password ${MYSQL_BACKUP_PW} --dbname paradise_gamedb --backuppath /data/mysql_backups/gamedb
+backup_mysql.sh --host 172.28.0.14 --user backup_user --password ${MYSQL_BACKUP_PW} --dbname paradise_gamedb --backuppath /data/mysql_backups/gamedb
 # PR DB
 backup_mysql.sh --host 172.28.0.14 --user backup_user --password ${MYSQL_BACKUP_PW} --dbname aa07_pr_voting --backuppath /data/mysql_backups/prs
 # TGS DB
-backup_mysql.sh --host 10.0.0.10 --user backup_user --password ${MYSQL_BACKUP_PW} --dbname paradise_tgs_espresso3 --backuppath /data/mysql_backups/tgs
+backup_mysql.sh --host 172.28.0.14 --user backup_user --password ${MYSQL_BACKUP_PW} --dbname paradise_tgs_espresso3 --backuppath /data/mysql_backups/tgs
 # Wiki DB
 backup_mysql.sh --host 172.28.0.14 --user backup_user --password ${MYSQL_BACKUP_PW} --dbname paradise_wiki --backuppath /data/mysql_backups/wiki
